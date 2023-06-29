e22fffc695ea7ec1aad3d5402954e687e28634e2
JoshuaImeze <joshuaimeze@gmail.com>
2023-06-29 12:23:30 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,50 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * main - check the code
+ * print_number - function that print integer
+ * @n: argument that be printed
  *
- * Return: Always 0.
+ * Description: print any given integer
+ *
+ * Return: return value
  */
-int main(void)
+void print_number(int n)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
+	int half;
+	int half2;
+
+	if (n < 0 && n > -2147483648)
+	{
+		_putchar('-');
+		n = -n;
+		if (n / 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+	else if (n == -2147483648)
+	{
+		half = (n / -1000);
+		half2 = (n % 1000);
+		half2 *= -1;
+		_putchar('-');
+		_putchar((half / 1000000) + '0');
+		_putchar(((half % 1000000) / 100000) + '0');
+		_putchar((((half % 1000000) % 100000) / 10000) + '0');
+		_putchar(((((half % 1000000) % 100000) % 10000) / 1000)  + '0');
+		_putchar((((((half % 1000000) % 100000) % 10000) % 1000) / 100)  + '0');
+		_putchar(((((((half % 1000000) % 100000)
+								% 10000) % 1000) % 100) / 10)  + '0');
+		_putchar((half % 10) + '0');
+		_putchar((half2 / 100) + '0');
+		_putchar(((half2 % 100) / 10) + '0');
+		_putchar((half2 % 10) + '0');
+	}
+	else
+	{
+		if (n / 10)
+		{
+			print_number(n / 10);
+		}
+		_putchar(n % 10 + '0');
+	}
 }
